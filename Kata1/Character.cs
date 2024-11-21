a1af4a9fd55eb44b18bb2aaa0f7237835d4295ff
Sinow89 <christoffer.rozenbachs@gmail.com>
2024-11-21 12:40:14 +0100

    Kata1

--- Kata1/Character.cs
+++ Kata1/Character.cs
@@ -1,6 +1,32 @@
-﻿namespace Kata1;
+﻿using System.Security.Cryptography;
 
-public class Character
+namespace Kata1;
+
+public  class Character
 {
+    public string Name { get; private set; }
+    public string Type { get; private set; }
+    public int Health { get; private set; }
+    public int Damage { get; private set; }
+    public Action PrimaryAction { get;  set; }
+    
+    public Character(string name, string type, int health, int damage, Action primaryAction)
+    {
+        Name = name;
+        Type = type;
+        Health = health;
+        Damage = damage;
+        PrimaryAction = primaryAction;
+    }
+    
+    public void Attack()
+    {
+        Console.WriteLine($"{Name} attacks Orc ");
+    }
+    
+    public void Heal(Character character)
+    {
+        Console.WriteLine($"{Name} heals {character.Name} ");
+    }
     
 }
--- Kata1/Program.cs
+++ Kata1/Program.cs
@@ -1,9 +1,30 @@
-﻿namespace Kata1;
+﻿using System.Security.Authentication;
+
+namespace Kata1;
 
 class Program
 {
     static void Main()
     {
-        C
+        Character Warrior = new ("Nisse", "Warrior", 49, 10, primaryAction: () => {Console.WriteLine("Attacking");});
+        Character Healer = new ("Kalle", "Healer", 45, 10,primaryAction: () => {Console.WriteLine("Attacking");});
+
+        List<Character> characters = new List<Character> { Warrior, Healer };
+        
+        var lowHealthCharacters = characters.Where(character => character.Health < 50).ToList();
+        foreach (var character in lowHealthCharacters)
+        {
+            Console.WriteLine($"{character.Name} is attacking first due to low health!");
+            character.PrimaryAction();
+            Warrior.Attack();
+            Healer.Heal(character);
+        }
+        Console.WriteLine();
     }
+
+    
+
+
+
+    
 }
