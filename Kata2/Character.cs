1685bc1e3fd36883c2203787c9c32818155ca7e9
Sinow89 <christoffer.rozenbachs@gmail.com>
2024-11-24 15:28:54 +0100

    Kata2

---
+++ Kata2/Character.cs
@@ -0,0 +1,8 @@
+﻿namespace Kata2;
+
+public class Character(string name, int health)
+{
+
+    public string Name { get; private set; } = name;
+    public int Health { get; private set; } = health;
+}
---
+++ Kata2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Kata2;
+
+class Program
+{
+    static void Main()
+    {
+        var character = new Character("Arin", 100);
+        var character2 = new Character("Dalia", 100);
+    }
+}
