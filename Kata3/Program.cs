b2570d9031d5cc540ec54fad108f729bc2483740
Sinow89 <christoffer.rozenbachs@gmail.com>
2024-11-25 10:03:15 +0100

    Kata3

---
+++ Kata3/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Kata3;
+
+/*
+ Expected Outpyt:
+ 
+ *Adding abilities to the container...
+- Added Attack: Slash Attack (Effect: Deals 15 damage)
+- Added Heal: Healing Light (Effect: Restores 20 health)
+
+Executing abilities...
+- Using Slash Attack on Enemy. Enemy health is now 85.
+- Using Healing Light on Ally. Ally health is now 120.
+
+Listing all abilities in the container:
+- Slash Attack (Effect: Deals 15 damage)
+- Healing Light (Effect: Restores 20 health)
+
+ */
+
+class Program
+{
+    static void Main()
+    {
+        
+    }
+}
